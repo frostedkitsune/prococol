e73800b534777289417b009174dd756a55f375d3
lazy34 <maitysurajit0901@gmail.com>
2024-05-03 11:34:36 +0530

    initial commit

---
+++ islands/Nav.tsx
@@ -0,0 +1,22 @@
+
+
+
+export const Nav=()=>{
+    return(
+        <>
+        <style>
+            @import url(/compostyle/Nav.css) 
+        </style> 
+        <div id="Nav-Bar">
+            <div className="Nav-logo-section">
+
+            </div>
+            <div className="Nav-appearance-section">
+                <div id="Nav-language" class="Nav-appearance-childs"></div>
+                <div id="Nav-mode-change" class="Nav-appearance-childs"></div>
+                <div id="Nav-starts" class="Nav-appearance-childs"></div>
+            </div>
+        </div>
+        </>
+    );
+}
